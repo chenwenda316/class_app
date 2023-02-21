f05d775b369beb7f78e3e75f7be7ebfb49a59c95
for_each316 <2014214637@qq.com>
2023-02-22 01:04:05 +0800

    ???

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,9 @@
+/*
+ * @Author: chenwenda316
+ * @Date: 2022-12-03 22:10:15
+ * @LastEditTime: 2023-02-22 01:03:37
+ * @FilePath: \class_app_dev\src\store\index.js
+ */
 import { defineStore } from 'pinia'
 
 export const store = defineStore('mainstore', {
