e12f011b0f2c718527f9f43bf63ee04c7f00abb2
for_each316 <2014214637@qq.com>
2023-09-08 22:49:28 +0800

    add jinrishici

--- src/views/ToolsView.vue
+++ src/views/ToolsView.vue
@@ -1,14 +1,50 @@
 <!--
  * @Author: chenwenda316
  * @Date: 2023-01-12 21:44:39
- * @LastEditTime: 2023-01-14 10:21:49
- * @FilePath: \app\class_app_dev\src\views\ToolsView.vue
+ * @LastEditTime: 2023-09-08 22:43:38
+ * @FilePath: \class_app_dev\src\views\ToolsView.vue
 -->
 <template>
-    <n-card :bordered="false" title = "欢迎！" style="backdrop-filter: blur(10px);background-color:rgba(255,255,255,0.8)">
+  <n-card :bordered="false" title="欢迎！" style="backdrop-filter: blur(10px);background-color:rgba(255,255,255,0.8)">
     <template #header-extra>
-      V2.5
+      V2.8
     </template>
-    施工中
-</n-card>
+    <!-- {{ title }}<br/>{{ author }}<br/><span v-for="(item) in content"> {{item}}<br/></span> -->
+    {{ main }}
+  </n-card>
 </template>
+
+<script>
+import { ref } from "vue";
+import {
+  defineComponent,
+} from "vue";
+
+import { load } from "jinrishici";
+export default defineComponent({
+  data() {
+    return {
+      title: "",
+      author: "",
+      content: "",
+      main:""
+    }
+  },
+  methods: {
+    loadSentence: function () {
+      load(result => {
+        this.main = result.data.content
+        this.title = result.data.origin.title
+        this.author = "[" + result.data.origin.dynasty +'] ' + result.data.origin.author
+        this.content = result.data.origin.content
+      }, err => {
+        console.log("test");
+      })
+    }
+  },
+  mounted: function () {
+    console.log(111);
+    this.loadSentence();
+  }
+})
+</script>
